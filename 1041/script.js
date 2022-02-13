748f3203ee405d3bff614eac6f0567c9b0e4062a
Gabriel <gcamposlopes@gmail.com>
2022-02-13 18:12:13 -0300

    resolution 1041

---
+++ 1041/script.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split(' ').map(item => parseFloat(item));
+var [x, y] = lines;
+if(x === 0 && y === 0)
+  console.log("Origem");
+else if(y === 0)
+  console.log("Eixo X");
+else if(x === 0)
+  console.log("Eixo Y");
+else if(x > 0 && y > 0)
+  console.log("Q1");
+else if(x < 0 && y > 0)
+  console.log("Q2");
+else if(x < 0 && y < 0)
+  console.log("Q3");
+else //x > 0 && y < 0
+  console.log("Q4");
