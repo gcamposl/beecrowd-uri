f3b68c6662146cd3bcddf461cc74b85d2eb3942d
Gabriel <gcamposlopes@gmail.com>
2022-02-16 09:18:33 -0300

    resolution 1045

--- src/init/1045/script.js
+++ src/init/1045/script.js
@@ -1,12 +1,13 @@
 let input = require('fs').readFileSync('stdin', 'utf8');
-let triangulo = input.split(" ").map(item => parseInt(item));
+let triangulo = input.split(" ").map(item => parseFloat(item));
 //triangulo.sort((a,b) => b - a);
 const teste = function (triangulo) {
+  let temp;
   const len = triangulo.length;
   for (let i = 0; i < len; i++) {
     for(let j = 0; j < len; j++) {
-      if(triangulo[j] > triangulo[j+1]){
-        let temp = triangulo[j];
+      if(triangulo[j] < triangulo[j+1]){
+        temp = triangulo[j];
         triangulo[j] = triangulo[j+1];
         triangulo[j+1] = temp;
       }
@@ -15,4 +16,45 @@ const teste = function (triangulo) {
   return triangulo;
 }
 
-console.log(teste(triangulo));
+let [a, b, c] = teste(triangulo);
+//let [a, b, c] = triangulo.sort((a,b) => b - a);
+if (a >= (b + c)){
+  console.log("NAO FORMA TRIANGULO");
+} else {
+  if (Math.pow(a, 2) === (Math.pow(b, 2) + Math.pow(c, 2))){
+    console.log("TRIANGULO RETANGULO");
+  } 
+  if (Math.pow(a, 2) > (Math.pow(b, 2) + Math.pow(c, 2))){
+    console.log("TRIANGULO OBTUSANGULO");
+  } 
+  if (Math.pow(a, 2) < (Math.pow(b, 2) + Math.pow(c, 2))){
+    console.log("TRIANGULO ACUTANGULO");
+  } 
+  if (a === b && b === c){
+    console.log("TRIANGULO EQUILATERO");
+  } else if (a === b !== c || b === c !== a || a === c !== b){
+    console.log("TRIANGULO ISOSCELES");
+  }  
+}
+
+// Submetido
+// let triangulo = input.split(" ").map(item => parseFloat(item));
+// let [a, b, c] = triangulo.sort((a,b) => b - a);
+// if (a >= (b + c)){
+//   console.log("NAO FORMA TRIANGULO");
+// } else {
+//   if ((a*a) === ((b*b) + (c*c))){
+//     console.log("TRIANGULO RETANGULO");
+//   } 
+//   if ((a*a) > ((b*b) + (c*c))){
+//     console.log("TRIANGULO OBTUSANGULO");
+//   } 
+//   if ((a*a) < ((b*b) + (c*c))){
+//     console.log("TRIANGULO ACUTANGULO");
+//   } 
+//   if (a === b && b === c){
+//     console.log("TRIANGULO EQUILATERO");
+//   } if ((((a == b) && (a != c) || (b == c) && (b != a)) || ((a == c) && (b != c)))){
+//     console.log("TRIANGULO ISOSCELES");
+//   }  
+// }
