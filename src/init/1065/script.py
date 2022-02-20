05938aed8ba042f8202cc90a209c4b6f224ca1d0
Gabriel <gcamposlopes@gmail.com>
2022-02-20 11:28:57 -0300

    solved 1065 in python

---
+++ src/init/1065/script.py
@@ -0,0 +1,22 @@
+
+'''
+for i in range(4): #[0,1,2,3,4]
+    number = (int) = input()
+    if(number)
+'''
+
+'''
+entry = [int (i) for i in input().split('\n')]
+
+for i in entry:
+    if entry % 2 == 0:
+        quant = quant + 1
+print('{} valores pares'.format(quant))
+'''
+quant = 0
+
+for i in range(5):
+    number = int(input())
+    if number % 2 == 0:
+        quant += 1
+print(quant, 'valores pares')
