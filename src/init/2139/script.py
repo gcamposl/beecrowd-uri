e4e13cdb0da41a8a82219bcb5d74e98818569283
gcamposl <gcamposlopes@gmail.com>
2022-04-25 06:42:00 -0300

    fix 2139, but wrong answer 5 percent

--- src/init/2139/script.py
+++ src/init/2139/script.py
@@ -3,21 +3,24 @@
 
 months = [31,29,31,30,31,30,31,31,30,31,30,31]
 days = 0
+while 1:
+    try:
+        n = input().split(' ')
+        month = int(n[0])
+        day = int(n[1])
 
-n = input().split(' ')
-month = int(n[0])
-day = int(n[1])
-
-if day == 25 and month == 12:
-    print('E natal!')
-elif day == 24 and month == 12:
-    print("E vespera de natal!")
-elif day > 25 and month == 12:
-    print('Ja passou!')
-else:
-    for i in range(month - 1, len(months)):
-        days += months[i]
-    
-    days -= day
-    days -= 6
-    print(f"Faltam {days} dias para o natal!")
+        if day == 25 and month == 12:
+            print('E natal!')
+        elif day == 24 and month == 12:
+            print("E vespera de natal!")
+        elif day > 25 and month == 12:
+            print('Ja passou!')
+        else:
+            for i in range(month - 1, len(months)):
+                days += months[i]
+            
+            days -= day
+            days -= 6
+            print(f"Faltam {days} dias para o natal!")
+    except:
+        break
