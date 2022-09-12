b182b65a085806073b1307a2b614a978642db7df
gcamposl <gcamposlopes@gmail.com>
2022-09-12 10:53:26 -0300

    feat: best solution for 1143

--- src/init/1143/script.py
+++ src/init/1143/script.py
@@ -1,3 +1,4 @@
+# solução ruim
 n = int(input())
 
 if n > 1 and n < 1000:
@@ -6,7 +7,14 @@ if n > 1 and n < 1000:
     while i < n:
         for j in range(3):
             print(v, end=' ')
-            v = v * (i+1)
+            v = v * (i + 1)
         i += 1
         v = i + 1
-        print('\n') 
+        print('\n')
+
+# solução melhor
+
+n = int(input())
+
+for i in range(1, n + 1):
+    print('%d %d %d' % (i, i**2, i**3))
