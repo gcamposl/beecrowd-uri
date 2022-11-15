88d455fc7f736c0337fe49a898873f6386ee58ae
gcamposl <gcamposlopes@gmail.com>
2022-11-15 17:54:08 -0300

    sorry beecrowd

--- src/data_structure/1110/script.py
+++ src/data_structure/1110/script.py
@@ -1,16 +1,19 @@
-x = int(input())
+while True:
+    x = int(input())
+    if x == 0:
+        break
 
-queue = []
-for i in range(1, x + 1):
-    queue.append(i)
+    queue = []
+    for i in range(1, x + 1):
+        queue.append(i)
 
-queue2 = []
+    queue2 = []
 
-if len(queue) > 1:
-    for i in range(len(queue) - 1):
-        if(len(queue) > 1):
-            queue2.append(queue.pop(i))
-            queue.insert(len(queue), queue[i])
+    if len(queue) > 1:
+        for i in range(len(queue) - 1):
+            if(len(queue) > 1):
+                queue2.append(queue.pop(i))
+                queue.insert(len(queue), queue[i])
 
-print("Discarded cards: " + str(queue2).replace("[", "").replace("]", ""))
-print("Remaining card:", queue.pop())
+    print("Discarded cards: " + str(queue2).replace("[", "").replace("]", ""))
+    print("Remaining card:", queue.pop())
